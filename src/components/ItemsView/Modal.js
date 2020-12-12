3766a923f950aa5cf73ee41b51b5d6f4c0ce1be9
Stas Dudzik <stas.dudzik@gmail.com>
2020-12-12 23:51:33 +0100

    adding mock picture and lorem30 to item modal

--- src/components/ItemsView/Modal.js
+++ src/components/ItemsView/Modal.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   top: 50%;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  justify-items: center;
+  grid-template-columns: 1;
   transform: translateY(-50%);
   left: 0;
   right: 0;
@@ -21,10 +26,31 @@ const Text = styled.h1`
   text-align: center;
   color: black;
 `;
+const Recipe = styled.p`
+  text-align: justify;
+  width: 70%;
+  margin: 20px;
+`;
+
+const Image = styled.img``;
 
 const Modal = ({ closeModalFn }) => (
   <Wrapper>
     <Text>Details of the dish</Text>
+    <Image
+      src="https://picsum.photos/id/292/190/190"
+      alt="Girl in a jacket"
+      width="150"
+      height="150"
+    />
+    <Recipe>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsa
+      tempore deleniti possimus inventore adipisci, nihil animi cupiditate
+      recusandae nulla earum harum provident! Fugit atque accusamus eius, animi
+      recusandae debitis mollitia velit, illo quos maiores veritatis vel saepe
+      modi laudantium aliquid adipisci dolorum minima. Expedita nemo est dolorem
+      eveniet.
+    </Recipe>
     <button onClick={closeModalFn}> &#10799;</button>
   </Wrapper>
 );
